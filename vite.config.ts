6b68026c6659d60877b9635df0333f2e1c27e259
hnamhocit <hnamhocit@gmail.com>
2025-03-01 11:59:46 -0500

    change build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 import tailwindcss from '@tailwindcss/vite'
@@ -6,21 +5,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	build: {
-		lib: {
-			entry: resolve(__dirname, 'lib/main.js'),
-			name: 'renui',
-			fileName: 'renui',
-		},
-		rollupOptions: {
-			external: ['react', 'react-dom', 'react/jsx-runtime'],
-			output: {
-				globals: {
-					React: 'react',
-					ReactDOM: 'react-dom',
-					ReactJsxRuntime: 'react/jsx-runtime',
-				},
-			},
-		},
-	},
+	// build: {
+	// 	lib: {
+	// 		entry: resolve(__dirname, 'lib/main.js'),
+	// 		name: 'renui',
+	// 		fileName: 'renui',
+	// 	},
+	// 	rollupOptions: {
+	// 		external: ['react', 'react-dom', 'react/jsx-runtime'],
+	// 		output: {
+	// 			globals: {
+	// 				React: 'react',
+	// 				ReactDOM: 'react-dom',
+	// 				ReactJsxRuntime: 'react/jsx-runtime',
+	// 			},
+	// 		},
+	// 	},
+	// },
 })
